96c56c44b88f7201355ccdcadd7dbf3896fcd3e6
nicogiard <nicogiard@gmail.com>
2011-08-17 17:09:14 +0200

    refactoring de l'import

--- app/controllers/Operations.java
+++ app/controllers/Operations.java
@@ -1,12 +1,7 @@
 package controllers;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Scanner;
 
 import javax.activation.MimetypesFileTypeMap;
 
@@ -17,17 +12,15 @@ import models.Operation;
 import models.OperationImport;
 import models.Tag;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 
 import play.data.validation.Required;
 import play.data.validation.Valid;
 import play.db.jpa.JPA;
 import play.mvc.Controller;
+import utils.ImportCSVCaisseEpargne;
 
 public class Operations extends Controller {
-
-	static DateFormat dateFormatter = new SimpleDateFormat("dd/MM/yy");
 
 	public static void ajouter(Long compteId) {
 		String titre = "Ajouter";
@@ -52,7 +45,7 @@ public class Operations extends Controller {
 		render(titre, compte, operation, allTags);
 	}
 
-	public static void enregistrer(@Required @Valid Operation operation, String tags) {
+	public static void enregistrer(@Required @Valid Operation operation, String tags, Float oldMontant) {
 		if (validation.hasErrors()) {
 			params.flash();
 			validation.keep();
@@ -63,13 +56,13 @@ public class Operations extends Controller {
 			}
 		}
 
-		// TODO : Est ce qu'il ne faudrait pas ici faire le recalcul de tous les soldes des opérations arrivant après ?
+		if (oldMontant == null) {
+			oldMontant = 0F;
+		}
 		if (operation.type == ETypeOperation.DEBIT) {
-			operation.solde = operation.compte.solde - operation.montant;
-			operation.compte.solde = operation.solde;
+			operation.compte.solde = (operation.compte.solde + oldMontant) - operation.montant;
 		} else {
-			operation.solde = operation.compte.solde + operation.montant;
-			operation.compte.solde = operation.solde;
+			operation.compte.solde = (operation.compte.solde - oldMontant) + operation.montant;
 		}
 
 		operation.tags.clear();
@@ -99,9 +92,20 @@ public class Operations extends Controller {
 	}
 
 	public static void supprimer(Long compteId, Long operationId) {
-		JPA.em().createNativeQuery("DELETE FROM Operation_Tags ot WHERE ot.operation_id=?").setParameter(1, operationId).executeUpdate();
-		JPA.em().createNativeQuery("DELETE FROM Operation o WHERE o.id=?").setParameter(1, operationId).executeUpdate();
-		// TODO : Est ce qu'il ne faudrait pas ici faire le recalcul de tous les soldes des opérations arrivant après ?
+		Compte compte = Compte.findById(compteId);
+		notFoundIfNull(compte);
+		Operation operation = Operation.findById(operationId);
+		notFoundIfNull(operation);
+
+		if (ETypeOperation.DEBIT == operation.type) {
+			compte.solde = compte.solde + operation.montant;
+		} else {
+			compte.solde = compte.solde - operation.montant;
+		}
+
+		compte.save();
+		operation.delete();
+
 		Comptes.resume(compteId, null);
 	}
 
@@ -132,45 +136,8 @@ public class Operations extends Controller {
 
 		if (fichier != null && fichier.exists()) {
 			if (fichier.getName().endsWith(".csv") && "application/octet-stream".equals(new MimetypesFileTypeMap().getContentType(fichier))) {
-				try {
-					List<String> lignes = FileUtils.readLines(fichier, "ISO-8859-1");
-
-					// On commence a la ligne 4 car c'est comme ca dans le fichier de CE
-					for (int i = 4; i < lignes.size(); i++) {
-						String ligne = lignes.get(i);
-						Scanner scan = new Scanner(ligne).useDelimiter(";");
-
-						String date = scan.next();
-						String numero = scan.next();
-						String libelle = scan.next();
-						String debit = scan.next();
-						String credit = scan.next();
-						String detail = scan.next();
-
-						OperationImport operationImport = new OperationImport();
-						try {
-							operationImport.date = dateFormatter.parse(date);
-						} catch (ParseException e) {
-							e.printStackTrace();
-						}
-						operationImport.numero = numero;
-						operationImport.libelle = libelle;
-						if (StringUtils.isNotBlank(debit)) {
-							operationImport.montant = new Float(debit.replace(",", ".").replace("-", ""));
-							operationImport.type = ETypeOperation.DEBIT;
-						} else if (StringUtils.isNotBlank(credit)) {
-							operationImport.montant = new Float(credit.replace(",", "."));
-							operationImport.type = ETypeOperation.CREDIT;
-						}
-						operationImport.detail = detail;
-						operationImport.compte = compte;
-						operationImport.save();
-
-						scan.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				// TODO : faire du multi opérateur d'import
+				new ImportCSVCaisseEpargne().importer(fichier, compte);
 			} else {
 				validation.addError("file.nocsv", "Le fichier n'est pas de type csv");
 			}
@@ -199,11 +166,9 @@ public class Operations extends Controller {
 			operation.detail = operationImport.detail;
 			operation.compte = operationImport.compte;
 			if (operation.type == ETypeOperation.DEBIT) {
-				operation.solde = operation.compte.solde - operation.montant;
-				operation.compte.solde = operation.solde;
+				operation.compte.solde = operation.compte.solde - operation.montant;
 			} else {
-				operation.solde = operation.compte.solde + operation.montant;
-				operation.compte.solde = operation.solde;
+				operation.compte.solde = operation.compte.solde + operation.montant;
 			}
 			operation.etat = EEtatOperation.NONPOINTEE;
 			operation.compte.save();
