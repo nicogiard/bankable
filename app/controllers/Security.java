0a81ce113ae1f3aa577e044afca1fb42a4dac5b1
nicogiard <nicogiard@gmail.com>
2011-08-30 17:18:22 +0200

    connectedUser

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -8,4 +8,9 @@ public class Security extends Secure.Security {
 		User user = User.find("login=? and password=?", username, password).first();
 		return user != null ? true : false;
 	}
+
+	public static User connectedUser() {
+		User user = User.find("byLogin", connected()).<User> first();
+		return user;
+	}
 }
